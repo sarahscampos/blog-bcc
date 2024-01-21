302e670c2ca757d1ecb623c69882c84e4d7271f9
sarah-campos <sarahstephanycampos@gmail.com>
2024-01-20 22:47:02 -0300

    utilizacao navlink

--- src/componentes/MenuLink/index.js
+++ src/componentes/MenuLink/index.js
@@ -1,14 +1,18 @@
-import { Link, useLocation } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import styles from './MenuLink.module.css';
 
 export default function MenuLink({ children, rota }) {
-    const localizacao = useLocation();
 
     return (
-        <Link 
-            className={`${styles.link} ${localizacao.pathname === rota ? styles.link_destacado : ''}`} 
-            to={rota}>
+        <NavLink 
+            className={ ({ isActive }) => `
+                ${styles.link}
+                ${isActive ? styles.link_destacado : ""}
+            `} 
+            to={rota}
+            end
+        >
             {children}
-        </Link>
+        </NavLink>
     )
 };
