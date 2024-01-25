07775b94d4bb297542902f0a01c036e2af4b3954
sarah-campos <sarahstephanycampos@gmail.com>
2024-01-24 21:06:25 -0300

    pagina sobre-mim

--- src/paginas/SobreMim/index.js
+++ src/paginas/SobreMim/index.js
@@ -1,5 +1,24 @@
+import PostModelo from 'componentes/PostModelo';
+import fotoCapa from 'assets/sobre_mim_capa.png';
+import fotoSobreMim from 'assets/sobre_mim_foto.png';
+import styles from './SobreMim.module.css';
+
 export default function SobreMim() {
     return (
-        <h1>Sobre Mim</h1>
+        <PostModelo
+            fotoCapa={fotoCapa}
+            titulo='Sobre mim'
+        >
+            <h3 className={styles.subtitulo}>
+                Oi! Eu sou a Sarah :D
+            </h3>
+
+            <img 
+                src={fotoSobreMim}
+                alt="Foto da Sarah Campos"
+                className={styles.fotoSobreMim}
+            />
+            <p className={styles.paragrafo}>Paragrafos...........</p>
+        </PostModelo>
     )    
 };
