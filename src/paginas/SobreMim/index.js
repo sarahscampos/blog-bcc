90fcfe9e8fdc7e65174448135402875f1512357d
sarah-campos <sarahstephanycampos@gmail.com>
2024-01-27 03:08:01 -0300

    paragrafos sobre mim

--- src/paginas/SobreMim/index.js
+++ src/paginas/SobreMim/index.js
@@ -1,6 +1,7 @@
 import PostModelo from 'componentes/PostModelo';
 import fotoCapa from 'assets/sobre_mim_capa.png';
 import fotoSobreMim from 'assets/sobre_mim_foto.png';
+import gifHomer from 'assets/giphy.gif';
 import styles from './SobreMim.module.css';
 
 export default function SobreMim() {
@@ -18,7 +19,11 @@ export default function SobreMim() {
                 alt="Foto da Sarah Campos"
                 className={styles.fotoSobreMim}
             />
-            <p className={styles.paragrafo}>Paragrafos...........</p>
+            <p className={styles.paragrafo}>Sou do Rio de Janeiro, amo jogar videogame, aprender  sobre tecnologia e ouvir piadas ruins.</p>
+            <p className={styles.paragrafo}>Atualmente estou no 3° período de Ciência da Computação e iniciando meus estudos em Front-end.</p>
+            <p className={styles.paragrafo}>Meu intuito é que com esse blog você possa saber o que é abordado nas disciplinas de CC.</p>
+            <p className={styles.paragrafo}>Espero que goste e, lembre-se, nunca fica mais fácil, você só fica melhor. &#x1F463;</p>
+            <div className={styles.gif}><img src={gifHomer} alt='Gif Homer Simpson' /></div>
         </PostModelo>
     )    
 };
