9396ca4ffa186376af48d6670b7e6b9b3e8640c3
sarah-campos <sarahstephanycampos@gmail.com>
2024-01-28 00:08:27 -0300

    biblioteca react markdown

--- src/paginas/Post/index.js
+++ src/paginas/Post/index.js
@@ -1,10 +1,26 @@
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
+import posts from "json/posts.json"; //Array de objetos
+import PostModelo from "componentes/PostModelo";
+import ReactMarkdown from "react-markdown"; //Biblioteca
+import "./Post.css"
 
 export default function Post() {
     const parametros = useParams(); //useParams() retorna um objeto, que tem a propriedade id, que é um parametro de rota
-    console.log(parametros);
+    const post = posts.find((post) => {
+        return post.id === Number(parametros.id);
+    })
 
     return (
-        <h1>Post {parametros.id}</h1>
+        <PostModelo
+            fotoCapa={`/assets/posts/${post.id}/capa.png`}
+            titulo={post.titulo}
+        >
+            <div className="post-markdown-container">
+                <ReactMarkdown>
+                    {post.texto}
+                </ReactMarkdown>
+            </div>
+            
+        </PostModelo>
     )
 }
